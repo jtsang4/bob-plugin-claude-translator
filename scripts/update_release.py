cc1260e2f5e97b39db8c3b6abea6f46596ccdb80
jtsang4 <wtzeng1@gmail.com>
2023-04-10 01:34:37 +0800

    init

---
+++ scripts/update_release.py
@@ -0,0 +1,42 @@
+import sys
+import hashlib
+import json
+from pathlib import Path
+
+
+def update_appcast(version, desc):
+    release_file = Path(f'release/openai-translator-{version}.bobplugin')
+    assert release_file.is_file(), 'Release file not exist'
+    with open(release_file, 'rb') as f:
+        c = f.read()
+        file_hash = hashlib.sha256(c).hexdigest()
+    version_info = {
+        'version': version,
+        'desc': desc,
+        'sha256': file_hash,
+        'url': f'https://github.com/yetone/bob-plugin-openai-translator/releases/download/v{version}/{release_file.name}',
+        'minBobVersion': '0.5.0'
+    }
+    appcast_file = Path('appcast.json')
+    if appcast_file.is_file():
+        with open(appcast_file, 'r') as f:
+            appcast = json.load(f)
+    else:
+        appcast = dict(identifier='jtsang.claude.translator', versions=[])
+    appcast['versions'].insert(0, version_info)
+    with open(appcast_file, 'w') as f:
+        json.dump(appcast, f, ensure_ascii=False, indent=2)
+
+def update_info_json(version):
+    info_file = Path('src/info.json')
+    with open(info_file, 'r') as f:
+        info = json.load(f)
+    info['version'] = version
+    with open(info_file, 'w') as f:
+        json.dump(info, f, ensure_ascii=False, indent=2)
+
+if __name__ == '__main__':
+    version = sys.argv[1]
+    desc = sys.argv[2]
+    update_appcast(version, desc)
+    update_info_json(version)
---
+++ src/main.js
@@ -0,0 +1,196 @@
+//@ts-check
+
+const lang = require("./lang.js");
+const ClaudeModels = [
+    "claude-v1",
+    "claude-v1.0",
+    "claude-v1.2",
+    "claude-instant-v1",
+    "claude-instant-v1.0",
+];
+
+function supportLanguages() {
+    return lang.supportLanguages.map(([standardLang]) => standardLang);
+}
+
+/**
+ * @param {string} apiKey - The authentication API key.
+ * @returns {{
+*   "Content-Type": string;
+*   "x-api-key": string;
+* }} The header object.
+*/
+function buildHeader(apiKey) {
+    return {
+        "Content-Type": "application/json",
+        "x-api-key": apiKey,
+    };
+}
+
+/**
+ * @param {Bob.TranslateQuery} query
+ * @returns {string}
+*/
+function generatePrompts(query) {
+    let userPrompt = `Translate from ${lang.langMap.get(query.detectFrom) || query.detectFrom} to ${lang.langMap.get(query.detectTo) || query.detectTo}, only give the result`;
+
+    if (query.detectTo === "wyw" || query.detectTo === "yue") {
+        userPrompt = `翻译成${lang.langMap.get(query.detectTo) || query.detectTo}`;
+    }
+
+    if (
+        query.detectFrom === "wyw" ||
+        query.detectFrom === "zh-Hans" ||
+        query.detectFrom === "zh-Hant"
+    ) {
+        if (query.detectTo === "zh-Hant") {
+            userPrompt = "翻译成繁体白话文";
+        } else if (query.detectTo === "zh-Hans") {
+            userPrompt = "翻译成简体白话文";
+        } else if (query.detectTo === "yue") {
+            userPrompt = "翻译成粤语白话文";
+        }
+    }
+    if (query.detectFrom === query.detectTo) {
+        if (query.detectTo === "zh-Hant" || query.detectTo === "zh-Hans") {
+            userPrompt = "润色此句";
+        } else {
+            userPrompt = "polish this sentence";
+        }
+    }
+
+    userPrompt = `$${userPrompt}:\n\n"${query.text}" =>`
+
+    return userPrompt;
+}
+
+/**
+ * @param {typeof ClaudeModels[number]} model
+ * @param {Bob.TranslateQuery} query
+ * @returns {{ 
+ *  model: typeof ClaudeModels[number];
+ *  prompt: string;
+ *  max_tokens_to_sample: number;
+ *  stop_sequences: string[]
+ *  temperature: number;
+ * }}
+*/
+function buildRequestBody(model, query) {
+    const prompt = generatePrompts(query);
+    return {
+        model,
+        prompt: `\n\nHuman: ${prompt}\n\nAssistant: `,
+        max_tokens_to_sample: 2048,
+        stop_sequences: [
+            "\n\nHuman:"
+        ],
+        temperature: 0,
+    };
+}
+
+/**
+ * @param {Bob.Completion} completion
+ * @param {Bob.HttpResponse} result
+ * @returns {void}
+*/
+function handleError(completion, result) {
+    const { statusCode } = result.response;
+    const reason = (statusCode >= 400 && statusCode < 500) ? "param" : "api";
+    completion({
+        error: {
+            type: reason,
+            message: `接口响应错误 - ${result.data.detail}`,
+            addtion: JSON.stringify(result),
+        },
+    });
+}
+
+/**
+ * @param {Bob.Completion} completion
+ * @param {Bob.TranslateQuery} query
+ * @param {Bob.HttpResponse} result
+ * @returns {void}
+*/
+function handleResponse(completion, query, result) {
+    const { completion: resultText } = result.data;
+
+    if (!resultText) {
+        completion({
+            error: {
+                type: "api",
+                message: "接口未返回结果",
+                addtion: JSON.stringify(result),
+            },
+        });
+        return;
+    }
+
+    let targetText = resultText.trim();
+
+    if (targetText.startsWith('"') || targetText.startsWith("「")) {
+        targetText = targetText.slice(1);
+    }
+    if (targetText.endsWith('"') || targetText.endsWith("」")) {
+        targetText = targetText.slice(0, -1);
+    }
+
+    completion({
+        result: {
+            from: query.detectFrom,
+            to: query.detectTo,
+            toParagraphs: targetText.split("\n"),
+        },
+    });
+}
+
+/**
+ * @type {Bob.Translate}
+ */
+function translate(query, completion) {
+    if (!lang.langMap.get(query.detectTo)) {
+        completion({
+            error: {
+                type: "unsupportLanguage",
+                message: "不支持该语种",
+                addtion: "不支持该语种",
+            },
+        });
+    }
+
+    const { model, apiKeys, apiUrl } = $option;
+
+    const apiKeySelection = apiKeys.split(",").map(key => key.trim());
+    const apiKey = apiKeySelection[Math.floor(Math.random() * apiKeySelection.length)];
+    
+    const isChatGPTModel = ClaudeModels.includes(model);
+    const apiUrlPath = "/v1/complete";
+    
+    const header = buildHeader(apiKey);
+    const body = buildRequestBody(model, query);
+
+    (async () => {
+        const result = await $http.request({
+            method: "POST",
+            url: apiUrl + apiUrlPath,
+            header,
+            body,
+        });
+
+        if (result.error) {
+            handleError(completion, result);
+        } else {
+            handleResponse(completion, query, result);
+        }
+    })().catch((err) => {
+        completion({
+            error: {
+                type: err._type || "unknown",
+                message: err._message || "未知错误",
+                addtion: err._addition,
+            },
+        });
+    });
+}
+
+exports.supportLanguages = supportLanguages;
+exports.translate = translate;
